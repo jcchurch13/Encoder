388e01e90758616d6577ea390d3b44596ffb5a49
jcchurch13 <jcchurch@mit.edu>
2015-08-16 21:47:44 -0400

    teensy started

---
+++ Teensy_Stepper_Servo/Teensy_Stepper_Servo/Teensy_Stepper_Servo.ino
@@ -0,0 +1,15 @@
+/* a slightly more streamlined stepper-servo
+ *  implemented on teensy 3.1
+ */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
